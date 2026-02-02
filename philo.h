542361aa386f93b7dbf0d2d903ba67c9923ab33b
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2026-02-02 07:34:51 +0100

    initial structures added

--- philo.h
+++ philo.h
@@ -9,3 +9,43 @@
 /*   Updated: 2025/10/11 17:13:48 by tvithara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_rules
+{
+    int  num_of_philo;
+    long time_die;
+    long time_eat;
+    long time_sleep;
+    int  num_must_eat;
+
+    long start_time;
+    int  dead;
+
+    pthread_mutex_t death_mutex;
+    pthread_mutex_t print_mutex;
+    pthread_mutex_t *forks;
+}   t_rules;
+
+typedef struct s_philo
+{
+    int id;
+    int meals_eaten;
+    long time_of_last_meal;
+
+    pthread_t thread;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
+
+    t_rules *rules;
+}   t_philo;
+
+#endif
