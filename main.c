3b86832c179f2ec75595cc1ad718e97e9854fe23
Fabio Vitharana <fabio17.vitharana@gmail.com>
2026-02-19 22:01:29 +0100

    updated- not polished

--- main.c
+++ main.c
@@ -14,20 +14,31 @@
 
 int main(int ac, char **av)
 {
-    t_rules rules;
-    t_philo *philos;
-    pthread_t monitor;
+    t_rules		rules;
+	t_philo		*philos;
+	pthread_t	monitor;
+	int			i;
 
-    if (ac < 5 || ac > 6)
-        return (1);
-    if (init_rules(&rules, ac, av))
-        return (1);
-    if (init_philos(&rules, &philos))
-        return (1);
-    
-    pthread_create(&monitor, NULL, monitor_rotine, philos);
-    pthread_detach(monitor);
-
-    cleanup(&rules,philos);
-    return (0);
+	if (ac < 5 || ac > 6)
+		return (1);
+	if (init_rules(&rules, ac, av))
+		return (1);
+	if (init_philos(&rules, &philos))
+		return (cleanup(&rules, philos), 1);
+	i = 0;
+	while (i < rules.num_of_philo)
+	{
+		pthread_create(&philos[i].thread, NULL, philo_routine, &philos[i]);
+		i++;
+	}
+	pthread_create(&monitor, NULL, monitor_routine, philos);
+	pthread_join(monitor, NULL);
+	i = 0;
+	while (i < rules.num_of_philo)
+	{
+		pthread_join(philos[i].thread, NULL);
+		i++;
+	}
+	cleanup(&rules, philos);
+	return (0);
 }
