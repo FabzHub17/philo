be88de152c7360d5ccf770b9f11757be4278f3af
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2026-02-19 11:07:31 +0100

    updates

---
+++ init.c
@@ -0,0 +1,6 @@
+#include "philo.h"
+
+int init_rules(t_rules *rules, int ac, char **av)
+{
+    return (0);
+}
--- main.c
+++ main.c
@@ -9,3 +9,25 @@
 /*   Updated: 2025/10/11 17:14:07 by tvithara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "philo.h"
+
+int main(int ac, char **av)
+{
+    t_rules rules;
+    t_philo *philos;
+    pthread_t monitor;
+
+    if (ac < 5 || ac > 6)
+        return (1);
+    if (init_rules(&rules, ac, av))
+        return (1);
+    if (init_philos(&rules, &philos))
+        return (1);
+    
+    pthread_create(&monitor, NULL, monitor_rotine, philos);
+    pthread_detach(monitor);
+
+    cleanup(&rules,philos);
+    return (0);
+}
