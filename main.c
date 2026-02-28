1f9566fbb147fe020b9a71f57aaeb1ca2b6fee32
Thashiru Chandupa Fabio Vitharana <tvithara@e3r5p7.42roma.it>
2026-02-28 19:12:10 +0100

    updated

--- main.c
+++ main.c
@@ -66,7 +66,6 @@ int main(int ac, char **av)
 	if (ac < 5 || ac > 6)
 	{
 		printf("Error: wrong number of arguments\n");
-		printf("Usage: ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
 		return (1);
 	}
 	if (!init_args(&table, ac, av))
